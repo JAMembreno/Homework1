4a2b68f217da722344305f420e7f31d7cc5b734b
jamembreno <membrenojose15@gmail.com>
2018-09-14 12:20:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jamembreno/homework1/helloWorld.java
@@ -0,0 +1,13 @@
+package com.example.jamembreno.homework1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class helloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
